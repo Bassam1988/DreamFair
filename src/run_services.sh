829660c623294de6d7c38a806391f87f6447ad99
Bassam Suliman <bassam.suliman@forest-interactive.com>
2024-07-04 14:56:33 +0800

    gitignore

--- src/run_services.sh
+++ src/run_services.sh
@@ -45,7 +45,7 @@ start_consumer_service() {
     echo "Starting service in $SERVICE_DIR ...for $SERVICE_NAME"
     cd $SERVICE_DIR
     source env/bin/activate
-    nohup python3 $SERVICE_DIR > flask_$SERVICE_NAME.log 2>&1 &
+    nohup python3 $SERVICE_DIR/consumer.py > flask_$SERVICE_NAME.log 2>&1 &
     echo $! > flask_$SERVICE_NAME.pid
     deactivate
     echo "Service at $SERVICE_DIR started for $SERVICE_NAME"
@@ -60,9 +60,9 @@ start_flask_service /home/ubuntu/DreamFair/DreamFair/src/storyboard 5001
 start_flask_service /home/ubuntu/DreamFair/DreamFair/src/gateway 8000
 
 
-start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/storyboard/consumer.py storyboard
-start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/text_to_image_service/consumer.py t2m_consumer
-start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/text_to_text_service/consumer.py t2t_consumer
+start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/storyboard storyboard
+start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/text_to_image_service t2m_consumer
+start_consumer_service /home/ubuntu/DreamFair/DreamFair/src/text_to_text_service t2t_consumer
 
 echo "All services have been started."
 
