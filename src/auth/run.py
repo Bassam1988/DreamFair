12790f758210d19a0dff6bc7df18b08585730e49
Bassam Suliman <bassam.suliman@forest-interactive.com>
2024-07-15 17:06:23 +0800

    gevent

--- src/auth/run.py
+++ src/auth/run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'), port=5000)
+# if __name__ == '__main__':
+#     app.run(ssl_context=('cert.pem', 'key.pem'), port=5000)
--- src/gateway/run.py
+++ src/gateway/run.py
@@ -1,6 +1,14 @@
+from gevent.pywsgi import WSGIServer
+from gevent.ssl import SSLContext, PROTOCOL_TLS_SERVER
+
 from app import create_app
 
 app = create_app()
 
+
+ssl_context = SSLContext(PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain('cert.pem', 'key.pem')
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'), port=5013)
+    # app.run(ssl_context=('cert.pem', 'key.pem'), port=5013)
+    http_server = WSGIServer(('', 8000), app)
+    http_server.serve_forever()
