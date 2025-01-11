2e3fe25d7356c1c75aa7256bcd7900231e6c879f
Bassam1988 <bassamsuliman@gmail.com>
2025-01-11 11:41:57 +0100

    prompt

--- src/text_to_text_service/app/bl/text2text_bl.py
+++ src/text_to_text_service/app/bl/text2text_bl.py
@@ -125,8 +125,8 @@ def generate_script(data, db_session, for_consumer=False, retries=0):
     #     "the result should be exactly like this"\
     #     "'{\"script\":\"generated script\",\"storyboards\":{\"1\":\"scene: script part, description: generated storyboard 1\",\"2\":\"scene: script part, description: generated storyboard 2\", ...}}' "
     
-    prompt = f"You are an expert scriptwriter and storyboard artist.
-    Generate a script in the style of {script_style} for a video lasting {video_duration} inspired by the following synopsis: {synopsis}.\n"
+    prompt = f"You are an expert scriptwriter and storyboard artist."
+    "Generate a script in the style of {script_style} for a video lasting {video_duration} inspired by the following synopsis: {synopsis}.\n"
     "After generating the script generate the visual and textual descriptions for each scene's storyboard.\n"
     "where in Visual Description describe the visuals summarizing the entire story, including key characters, actions, and settings.\n"
     "adn in Textual Description provide a concise summary of the story's key moments.\n"
@@ -226,8 +226,8 @@ def generate_storyboard(data, db_session, for_consumer=False,retries=0):
     #     "'{\"storyboards\":{\"1\":\"scene: script part, description: generated storyboard 1\",\"2\":\"scene: script part, description: generated storyboard 2\", ...}}' \n"\
     #     f"this is the script: {script}."
 
-    prompt = f"You are an expert scriptwriter and storyboard artist. Based on the following script, 
-    generate visual and textual descriptions for each scene's storyboard.\n"
+    prompt = f"You are an expert scriptwriter and storyboard artist. Based on the following script," 
+    "generate visual and textual descriptions for each scene's storyboard.\n"
     "where in Visual Description describe the visuals summarizing the entire story, including key characters, actions, and settings.\n"
     "adn in Textual Description provide a concise summary of the story's key moments.\n"
     "like this:\n"
